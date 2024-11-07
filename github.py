5a35b045c5996f0f31513c564964ae20fb1b2017
Kunter Cetin <kunter@tek-metal.com>
2024-11-07 12:13:59 +0000

    First function

---
+++ github.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 11:52:30 2024
+
+@author: pc
+"""
+
+"This is the first python file i'll use git with."
+
+def hi_git_func():
+    return 'Hi GIT!!!'
